c696a69c12eb025d329f5d7ff759e79feb1e8b8f
flyingbucket <2311067@mail.nankai.edu.cn>
2026-04-23 20:58:23 +0800

    sync_a30_nvim: stop syncing ~/.local/state/nvim

--- sync_a30_nvim.sh
+++ sync_a30_nvim.sh
@@ -8,13 +8,11 @@ REMOTE_BASE="/home/${REMOTE_USER}"
 
 # 本地路径
 LOCAL_NVIM="$HOME/apps/neovim"
-LOCAL_STATE="$HOME/.local/state/nvim"
 LOCAL_SHARE="$HOME/.local/share/nvim"
 LOCAL_CONFIG="$HOME/dotfiles/nvim"
 
 # 远程路径
 REMOTE_APPS="${REMOTE_BASE}/apps/"
-REMOTE_STATE="${REMOTE_BASE}/.local/state/"
 REMOTE_SHARE="${REMOTE_BASE}/.local/share/"
 REMOTE_CONFIG="${REMOTE_BASE}/.config/"
 
@@ -23,9 +21,6 @@ RSYNC_OPTS="-ah --info=progress2"
 
 # echo "[*] Sync neovim binary..."
 # rsync $RSYNC_OPTS "$LOCAL_NVIM" "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_APPS}"
-
-echo "[*] Sync nvim state..."
-rsync $RSYNC_OPTS "$LOCAL_STATE" "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_STATE}"
 
 echo "[*] Sync nvim share (plugins, etc.)..."
 rsync $RSYNC_OPTS "$LOCAL_SHARE" "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_SHARE}"
